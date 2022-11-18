ee13301bad4f740460173e42023d3d2716e9025a
Carol-gh <quintanavargascarolselena@gmail.com>
2022-11-18 12:36:54 -0400

    1era forma2

--- js/core/watch.js
+++ js/core/watch.js
@@ -1,6 +1,6 @@
 'use strict'
 const io = require('socket.io-client');
-const socket = io.connect('localhost:8083');
+const socket = io.connect('http://54.91.43.203/:9090');
 console.log('socket cargado...');
 
 module.exports =  {
